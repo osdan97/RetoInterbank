51f38dc95e404b45784c0387e9bc487fcf1b5d7c
osdan97 <daniejurade@gmail.com>
2023-09-14 23:16:09 -0500

    empty

---
+++ Interbank-Challenge/src/main/java/pe/reto/interbank/InterbankChallengeApplication.java
@@ -0,0 +1,13 @@
+package pe.reto.interbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterbankChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterbankChallengeApplication.class, args);
+	}
+
+}
---
+++ Interbank-Challenge/src/test/java/pe/reto/interbank/InterbankChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.reto.interbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterbankChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
